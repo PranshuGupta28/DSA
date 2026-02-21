ead657d1e6402d07b7570c72095e28e77b2dc820
Pranshu Gupta <157245518+PranshuGupta28@users.noreply.github.com>
2026-02-22 00:27:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (86.25%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,108 +10,92 @@
  */
 class Solution {
 public:
-    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    //     ListNode*temp3=new ListNode(-1);
-    //     ListNode*head=temp3;
-    //     ListNode*temp1=list1;
-    //     ListNode*temp2=list2;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode*temp3=new ListNode(-1);
+        ListNode*head=temp3;
+        ListNode*temp1=list1;
+        ListNode*temp2=list2;
 
-    //     if(list1==NULL && list2==NULL){
-    //         return NULL;
-    //     }
-    //     if(list1==NULL && list2!=NULL){
-    //         return list2;
-    //     }
-    //     if(list1!=NULL && list2==NULL){
-    //         return list1;
-    //     }
+        // if(list1==NULL && list2==NULL){
+        //     return NULL;
+        // }
+        // if(list1==NULL && list2!=NULL){
+        //     return list2;
+        // }
+        // if(list1!=NULL && list2==NULL){
+        //     return list1;
+        // }
 
 
-    //     while(temp1->next!=NULL&& temp2->next!=NULL){
-    //         if(temp1->val<temp2->val){
-    //             temp3->next=temp1;
-    //             temp1=temp1->next;
-    //             temp3=temp3->next;
-    //         }
-    //         if(temp1->val>temp2->val){
-    //             temp3->next=temp2;
-    //             temp2=temp2->next;
-    //             temp3=temp3->next;
-    //         }
-    //         else{
-    //             temp3->next=temp1;
-    //             temp1=temp1->next;
-    //             temp3=temp3->next;
-    //         }
-    //     }
-    //     if(temp1->val>temp2->val){
-
-    //     while(temp2!=NULL){
-    //      temp3->next=temp2;
-    //      temp2=temp2->next;
-    //      temp3=temp3->next;
-    //         }
-    //     while(temp1!=NULL){
-    //      temp3->next=temp1;
-    //      temp1=temp1->next;
-    //      temp3=temp3->next;
-    //         }
-
-    //     }
-    //     else{
-
-    //         while(temp1!=NULL){
-    //      temp3->next=temp1;
-    //      temp1=temp1->next;
-    //      temp3=temp3->next;
-    //         }
-    //          while(temp2!=NULL){
-    //      temp3->next=temp2;
-    //      temp2=temp2->next;
-    //      temp3=temp3->next;
-    //         }
-    //     }
-
-    //     head=head->next;
-    //     return head;
-    // }
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
-        ListNode* dummyNode = new ListNode(-1);
-        ListNode* head = dummyNode;
-        ListNode* temp1 = list1;
-        ListNode* temp2 = list2;
-
-        while(temp1 != NULL && temp2 != NULL){
-            if(temp1->val < temp2->val){
-                dummyNode->next = temp1;
-                dummyNode = dummyNode->next;
-                temp1= temp1->next;
-                
+        while(temp1!=NULL&& temp2!=NULL){
+            if(temp1->val<temp2->val){
+                temp3->next=temp1;
+                temp3=temp3->next;
+                temp1=temp1->next;
             }
-
+            else if(temp1->val>temp2->val){
+                temp3->next=temp2;
+                temp3=temp3->next;
+                temp2=temp2->next;
+            }
             else{
-                dummyNode->next = temp2;
-                 dummyNode = dummyNode->next;
-                temp2 = temp2->next;
-               
+                temp3->next=temp1;
+                temp1=temp1->next;
+                temp3=temp3->next;
             }
         }
 
-        while(temp1 != NULL){
-            dummyNode->next = temp1;
-            temp1= temp1->next;
-            dummyNode = dummyNode->next;
-        }
+        while(temp1!=NULL){
+         temp3->next=temp1;
+         temp3=temp3->next;
+         temp1=temp1->next;
+            }
 
-        while(temp2 != NULL){
-           dummyNode->next = temp2;
-            temp2 = temp2->next;
-            dummyNode = dummyNode->next;
-        }
-        dummyNode = head;
+        while(temp2!=NULL){
+         temp3->next=temp2;
+         temp3=temp3->next;
+         temp2=temp2->next;
+            }
+
+        
+    
+    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+    //     ListNode* dummyNode = new ListNode(-1);
+    //     ListNode* head = dummyNode;
+    //     ListNode* temp1 = list1;
+    //     ListNode* temp2 = list2;
+
+    //     while(temp1 != NULL && temp2 != NULL){
+    //         if(temp1->val < temp2->val){
+    //             dummyNode->next = temp1;
+    //             dummyNode = dummyNode->next;
+    //             temp1= temp1->next;
+                
+    //         }
+
+    //         else{
+    //             dummyNode->next = temp2;
+    //              dummyNode = dummyNode->next;
+    //             temp2 = temp2->next;
+               
+    //         }
+    //     }
+
+    //     while(temp1 != NULL){
+    //         dummyNode->next = temp1;
+    //         temp1= temp1->next;
+    //         dummyNode = dummyNode->next;
+    //     }
+
+    //     while(temp2 != NULL){
+    //        dummyNode->next = temp2;
+    //         temp2 = temp2->next;
+    //         dummyNode = dummyNode->next;
+    //     }
+        temp3 = head;
         head= head->next;
-        dummyNode->next = NULL;
-        delete dummyNode;
+        temp3->next = NULL;
+        delete temp3;
         return head;
         
      }
