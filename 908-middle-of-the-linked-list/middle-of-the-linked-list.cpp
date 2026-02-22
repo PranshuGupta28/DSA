7ccbf5e49d81d822485b2521e5586534fb09e5de
Pranshu Gupta <157245518+PranshuGupta28@users.noreply.github.com>
2026-02-22 19:15:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (89.47%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -10,25 +10,45 @@
  */
 class Solution {
 public:
-   int findL(ListNode*head) {
-        int len=0;
-        ListNode*temp=head;
-        while(temp!=NULL){
-            temp=temp->next;
-            len++;
-        }
-        return len;
-    }
+//    int findL(ListNode*head) {
+//         int len=0;
+//         ListNode*temp=head;
+//         while(temp!=NULL){
+//             temp=temp->next;
+//             len++;
+//         }
+//         return len;
+//     }
 
 
+//     ListNode* middleNode(ListNode* head) {
+//           int h=findL(head)/2;
+//          int len=0;
+//          ListNode*temp=head;
+//          while(len<h){
+//             temp=temp->next;
+//             len++;
+//          }
+//          return temp;
+//     }
     ListNode* middleNode(ListNode* head) {
-         int h=findL(head)/2;
-         int len=0;
-         ListNode*temp=head;
-         while(len<h){
-            temp=temp->next;
-            len++;
-         }
-         return temp;
+        if(head == NULL){
+            return NULL;
+        }
+        if(head->next == NULL){
+            return head;
+        }
+        ListNode*slow = head;
+        ListNode*fast = head;
+
+        while(slow !=NULL && fast != NULL){
+            fast = fast->next;
+            if(fast != NULL){
+                fast = fast->next;
+                slow = slow->next;
+            }
+        }
+        return slow;
     }
+
 };
