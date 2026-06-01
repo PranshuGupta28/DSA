fb4e6c5130a78cd61717ca718e70fc504fe79257
Pranshu Gupta <157245518+PranshuGupta28@users.noreply.github.com>
2026-06-01 16:32:11 +0530

    Time: 267 ms (29.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+select machine_id ,  
+round (
+    ((sum(case when activity_type ='end' then timestamp end )-
+    sum(case when activity_type ='start'then timestamp end))/
+    count(distinct process_id))
+    ,3)
+ as processing_time
+from activity
+group by machine_id
+
+
+
+-- select round (sum(case when activity_type ='end' then timestamp end )-
+-- sum(case when activity_type ='start'then timestamp end),3) / 
+-- (select count(*)
+-- from activity
+-- group by process_id)
+-- from activity 
+-- group by machine_id
